5233a2838dd7cbc65eed695a4635e5a57f872e44
harmonie-durrant <harmoniedurrant@gmail.com>
2024-10-10 14:44:57 +0200

    v1.5

--- kubejs/server_scripts/recipes/remove.js
+++ kubejs/server_scripts/recipes/remove.js
@@ -2,11 +2,7 @@ ServerEvents.recipes(event => {
     const removals = [
         {
             id: /refinedstorage:(?!controller|cable|crafting_grid|external_storage|quartz_enriched_iron|quartz_enriched_iron_block)\w+/
-        },
-        {
-            mod: 'theoneprobe'
-        },
-        { mod: 'simplybackpacks' }
+        }
     ];
     removals.forEach((removal) => {
         event.remove(removal);
